592d0219ed07ebfea3d0e2046ca1872d1eca8935
Rudrank Riyam <rudrankriyam@gmail.com>
2021-08-04 01:31:51 +0530

    Create an Xcode project and add platforms

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "RRMusicKit",
+    platforms: [.iOS(.v15), .macOS(.v12), .watchOS(.v8), .tvOS(.v15)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
