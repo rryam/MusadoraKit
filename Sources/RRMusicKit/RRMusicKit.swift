8e0824742d4fabb352d59d3fc4c9b072322b2cfb
Rudrank Riyam <rudrankriyam@gmail.com>
2021-08-04 01:24:34 +0530

    Initial commit

---
+++ Sources/RRMusicKit/RRMusicKit.swift
@@ -0,0 +1,6 @@
+public struct RRMusicKit {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/RRMusicKitTests/RRMusicKitTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import RRMusicKit
+
+final class RRMusicKitTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(RRMusicKit().text, "Hello, World!")
+    }
+}
