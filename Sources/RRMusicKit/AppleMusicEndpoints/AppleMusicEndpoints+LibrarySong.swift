3d18a355b2aa11db1fc313772c90e589fda6c1ef
Rudrank Riyam <rudrankriyam@gmail.com>
2021-08-04 08:29:27 +0530

    Add AppleMusicEndpoints and Catalog Songs

---
+++ Sources/RRMusicKit/AppleMusicEndpoints/AppleMusicEndpoints+LibrarySong.swift
@@ -0,0 +1,23 @@
+//
+//  AppleMusicEndpoints+LibrarySong.swift
+//  AppleMusicEndpoints+LibrarySong
+//
+//  Created by Rudrank Riyam on 04/08/21.
+//
+
+import Foundation
+
+// MARK: - REQUESTING A LIBRARY SONG
+public extension AppleMusicEndPoint {
+    static func getLibrarySong(id: String) -> Self {
+        AppleMusicEndPoint(library: .user, path: "/library/songs/\(id)", addStoreFront: false)
+    }
+    
+    static func getAllLibrarySongs(id: String) -> Self {
+        AppleMusicEndPoint(library: .user, path: "/library/songs", addStoreFront: false)
+    }
+    
+    static var userLibrary: Self {
+        AppleMusicEndPoint(library: .user, path: "library", addStoreFront: false)
+    }
+}
