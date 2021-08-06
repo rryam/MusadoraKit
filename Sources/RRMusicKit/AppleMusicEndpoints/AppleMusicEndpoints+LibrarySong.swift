5ff44d9d9693da2eb9446b5cbbbee06de80e0f2d
Rudrank Riyam <rudrankriyam@gmail.com>
2021-08-06 16:11:52 +0530

    Update AppleMusicEndPoint

--- Sources/RRMusicKit/AppleMusicEndpoints/AppleMusicEndpoints+LibrarySong.swift
+++ Sources/RRMusicKit/AppleMusicEndpoints/AppleMusicEndpoints+LibrarySong.swift
@@ -10,14 +10,14 @@ import Foundation
 // MARK: - REQUESTING A LIBRARY SONG
 public extension AppleMusicEndPoint {
     static func getLibrarySong(id: String) -> Self {
-        AppleMusicEndPoint(library: .user, path: "/library/songs/\(id)", addStoreFront: false)
+        AppleMusicEndPoint(library: .user, "/library/songs/\(id)", addStoreFront: false)
     }
     
     static func getAllLibrarySongs(id: String) -> Self {
-        AppleMusicEndPoint(library: .user, path: "/library/songs", addStoreFront: false)
+        AppleMusicEndPoint(library: .user, "/library/songs", addStoreFront: false)
     }
     
     static var userLibrary: Self {
-        AppleMusicEndPoint(library: .user, path: "library", addStoreFront: false)
+        AppleMusicEndPoint(library: .user, "library", addStoreFront: false)
     }
 }
