1810d66ebebe9e16ef728dda07b630f7fdf0bb31
Rudrank <rudrankriyam@gmail.com>
2021-09-08 22:59:53 +0530

    Add CatalogMusicVideo

--- Sources/MusadoraKit/Catalog/CatalogAlbum.swift
+++ Sources/MusadoraKit/Catalog/CatalogAlbum.swift
@@ -11,30 +11,37 @@ public typealias Albums = MusicItemCollection<Album>
 
 public extension MusadoraKit {
     static func catalogAlbum(id: MusicItemID) async throws -> Albums {
-        let musicRequest = MusicCatalogResourceRequest<Album>(matching: \.id, equalTo: id)
-        let response = try await musicRequest.response()
+        let request = MusicCatalogResourceRequest<Album>(matching: \.id, equalTo: id)
+        let response = try await request.response()
         
         return response.items
     }
     
     static func catalogAlbums(ids: [MusicItemID]) async throws -> Albums {
-        let musicRequest = MusicCatalogResourceRequest<Album>(matching: \.id, memberOf: ids)
-        let response = try await musicRequest.response()
+        let request = MusicCatalogResourceRequest<Album>(matching: \.id, memberOf: ids)
+        let response = try await request.response()
+        
+        return response.items
+    }
+    
+    static func catalogAlbums(isrc: String) async throws -> Albums {
+        let request = MusicCatalogResourceRequest<Album>(matching: \.upc, equalTo: isrc)
+        let response = try await request.response()
         
         return response.items
     }
     
     static func catalogAlbums(isrc: [String]) async throws -> Albums {
-        let musicRequest = MusicCatalogResourceRequest<Album>(matching: \.upc, memberOf: isrc)
-        let response = try await musicRequest.response()
+        let request = MusicCatalogResourceRequest<Album>(matching: \.upc, memberOf: isrc)
+        let response = try await request.response()
         
         return response.items
     }
     
     static func catalogAlbum(id: MusicItemID, with relationships: [PartialMusicAsyncProperty<Album>]) async throws -> Albums {
-        var musicRequest = MusicCatalogResourceRequest<Album>(matching: \.id, equalTo: id)
-        musicRequest.properties = relationships
-        let response = try await musicRequest.response()
+        var request = MusicCatalogResourceRequest<Album>(matching: \.id, equalTo: id)
+        request.properties = relationships
+        let response = try await request.response()
         
         return response.items
     }
