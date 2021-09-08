aaeb660fa7622e041565a3c9b6ee3921bca6cd03
Rudrank <rudrankriyam@gmail.com>
2021-09-08 23:08:13 +0530

    Add CatalogPlaylists

--- Sources/MusadoraKit/Catalog/CatalogPlaylist.swift
+++ Sources/MusadoraKit/Catalog/CatalogPlaylist.swift
@@ -8,3 +8,27 @@
 import MusicKit
 
 public typealias Playlists = MusicItemCollection<Playlist>
+
+public extension MusadoraKit {
+    static func playlist(id: MusicItemID) async throws -> Playlists {
+        let request = MusicCatalogResourceRequest<Playlist>(matching: \.id, equalTo: id)
+        let response = try await request.response()
+        
+        return response.items
+    }
+    
+    static func playlists(ids: [MusicItemID]) async throws -> Playlists {
+        let request = MusicCatalogResourceRequest<Playlist>(matching: \.id, memberOf: ids)
+        let response = try await request.response()
+        
+        return response.items
+    }
+    
+    static func playlist(id: MusicItemID, with relationships: [PartialMusicAsyncProperty<Playlist>]) async throws -> Playlists {
+        var request = MusicCatalogResourceRequest<Playlist>(matching: \.id, equalTo: id)
+        request.properties = relationships
+        let response = try await request.response()
+        
+        return response.items
+    }
+}
