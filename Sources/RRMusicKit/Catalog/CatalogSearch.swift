8f108d33da822d32825625d81c58a5d2a2c6a276
Rudrank <rudrankriyam@gmail.com>
2021-09-08 18:13:50 +0530

    Update catalogSearch

--- Sources/RRMusicKit/Catalog/CatalogSearch.swift
+++ Sources/RRMusicKit/Catalog/CatalogSearch.swift
@@ -8,63 +8,15 @@
 import Foundation
 import MusicKit
 
-public enum SearchTypes: String {
-    case activities
-    case albums
-    case appleCurators
-    case artists
-    case curators
-    case musicVideos
-    case playlists
-    case recordLabels
-    case songs
-    case stations
-    
-    var type: String {
-        switch self {
-            case .appleCurators: return "apple-curators"
-            case .musicVideos: return "music-videos"
-            case .recordLabels: return "record-labels"
-            default: return rawValue
-        }
-    }
-}
-
-extension AppleMusicEndpoint {
-    static func search(for term: String, limit: Int, types: [SearchTypes]) -> Self {
-        let termQuery = URLQueryItem(name: "term", value: term.replacingOccurrences(of: " ", with: "+"))
-        let types = types.map { String($0.type) }.joined(separator: ",")
-        
-        let typesQuery = URLQueryItem(name: "types", value: types)
-        
-        let limitQuery = URLQueryItem(name: "limit", value: String(describing: limit))
-        
-        let queryItems = [termQuery, limitQuery, typesQuery]
-        
-        return AppleMusicEndpoint(library: .catalog, "search", queryItems: queryItems)
-    }
-}
-
-#warning("The search API returns a mix of a lot of stuff. Need to figure out a way to do that.")
 public extension RRMusicKit {
-    static func search(for term: String, limit: Int = 20, types: [SearchTypes] = [.songs]) async throws -> [Song] {
-        let model: SearchSongModel = try await self.decode(endpoint: .search(for: term, limit: limit, types: types))
-        return model.results.songs.data
+    static func catalogSearch(for term: String,
+                       types: [MusicCatalogSearchable.Type],
+                       limit: Int?,
+                       offset: Int?) async throws -> MusicCatalogSearchResponse {
+        var request = MusicCatalogSearchRequest(term: term, types: types)
+        request.limit = limit
+        request.offset = offset
+        
+        return try await request.response()
     }
-}
-
-
-// MARK: - SearchSongModel
-public struct SearchSongModel: Codable {
-    let results: SearchSongResults
-}
-
-// MARK: - SearchSongResults
-public struct SearchSongResults: Codable {
-    let songs: SearchSongs
-}
-
-// MARK: - WelcomeResults
-public struct SearchSongs: Codable {
-    let data: [Song]
 }
