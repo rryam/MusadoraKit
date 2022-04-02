2d8f851ca932a74f845af88b239e1178f953fdea
Rudrank Riyam <rudrankriyam@gmail.com>
2022-04-02 11:54:15 +0530

    [WIP] Add MusicLibraryResourceRequest and MusicLibraryResourceResponse

---
+++ Sources/MusadoraKit/Library/MusicLibraryResourceRequest/MusicLibraryResourceResponse.swift
@@ -0,0 +1,65 @@
+//
+//  MusicLibraryResourceResponse.swift
+//  MusicLibraryResourceResponse
+//
+//  Created by Rudrank Riyam on 02/04/22.
+//
+
+import MusicKit
+
+/// An object that contains results for a library resource request.
+@available(iOS 15.0, macOS 12.0, tvOS 15.0, watchOS 8.0, *)
+public struct MusicLibraryResourceResponse<MusicItemType> where MusicItemType: MusicItem {
+
+    /// A collection of items matching the filter used in
+    /// the originating ``MusicLibraryResourceRequest``.
+    public let items: MusicItemCollection<MusicItemType>
+}
+
+@available(iOS 15.0, macOS 12.0, tvOS 15.0, watchOS 8.0, *)
+extension MusicLibraryResourceResponse: Equatable where MusicItemType: Equatable {
+}
+
+@available(iOS 15.0, macOS 12.0, tvOS 15.0, watchOS 8.0, *)
+extension MusicLibraryResourceResponse: Hashable where MusicItemType: Hashable {
+}
+
+@available(iOS 15.0, macOS 12.0, tvOS 15.0, watchOS 8.0, *)
+extension MusicLibraryResourceResponse: Decodable where MusicItemType: Decodable {
+
+    /// Creates a new instance by decoding from the given decoder.
+    ///
+    /// This initializer throws an error if reading from the decoder fails, or
+    /// if the data read is corrupted or otherwise invalid.
+    ///
+    /// - Parameter decoder: The decoder to read data from.
+//    public init(from decoder: Decoder) throws {
+//
+//    }
+}
+
+@available(iOS 15.0, macOS 12.0, tvOS 15.0, watchOS 8.0, *)
+extension MusicLibraryResourceResponse: Encodable where MusicItemType: Encodable {
+
+    /// Encodes this value into the given encoder.
+    ///
+    /// If the value fails to encode anything, `encoder` will encode an empty
+    /// keyed container in its place.
+    ///
+    /// This function throws an error if any values are invalid for the given
+    /// encoder's format.
+    ///
+    /// - Parameter encoder: The encoder to write data to.
+   // public func encode(to encoder: Encoder) throws { }
+}
+
+@available(iOS 15.0, macOS 12.0, tvOS 15.0, watchOS 8.0, *)
+extension MusicLibraryResourceResponse: CustomStringConvertible, CustomDebugStringConvertible {
+    public var description: String {
+        ""
+    }
+
+    public var debugDescription: String {
+        ""
+    }
+}
