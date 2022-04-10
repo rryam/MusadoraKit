d0294aa4ab8d5ce663e02ff9e435462f24358d2c
Rudrank Riyam <rudrankriyam@gmail.com>
2022-04-10 19:04:52 +0530

    Add catalog genre

--- Sources/MusadoraKit/Catalog/CatalogGenre.swift
+++ Sources/MusadoraKit/Catalog/CatalogGenre.swift
@@ -8,6 +8,10 @@
 import Foundation
 import MusicKit
 
+/// A collection of genres.
+@available(iOS 15.0, macOS 12.0, tvOS 15.0, watchOS 8.0, *)
+public typealias Genres = MusicItemCollection<Genre>
+
 @available(iOS 15.0, macOS 12.0, tvOS 15.0, watchOS 8.0, *)
 extension AppleMusicEndpoint {
     static var genres: Self {
@@ -17,7 +21,34 @@ extension AppleMusicEndpoint {
 
 @available(iOS 15.0, macOS 12.0, tvOS 15.0, watchOS 8.0, *)
 public extension MusadoraKit {
-    static func genres() async throws -> MusicItemCollection<Genre> {
-        try await self.decode(endpoint: .genres)
+
+    /// Fetch a genre from the Apple Music catalog by using its identifier.
+    /// - Parameters:
+    ///   - id: The unique identifier for the genre.
+    ///   - properties: Additional relationships to fetch with the genre.
+    /// - Returns: `Genre` matching the given identifier.
+    static func catalogGenre(id: MusicItemID,
+                             with properties: [PartialMusicAsyncProperty<Genre>] = []) async throws -> Genre {
+        var request = MusicCatalogResourceRequest<Genre>(matching: \.id, equalTo: id)
+        request.properties = properties
+        let response = try await request.response()
+
+        guard let genre = response.items.first else {
+            throw MusadoraKitError.notFound(for: id.rawValue)
+        }
+        return genre
+    }
+
+    /// Fetch multiple genres from the Apple Music catalog by using their identifiers.
+    /// - Parameters:
+    ///   - ids: The unique identifiers for the genres.
+    ///   - properties: Additional relationships to fetch with the genres.
+    /// - Returns: `Genres` matching the given identifiers.
+    static func catalogGenres(ids: [MusicItemID],
+                              with properties: [PartialMusicAsyncProperty<Genre>] = []) async throws -> Genres {
+        var request = MusicCatalogResourceRequest<Genre>(matching: \.id, memberOf: ids)
+        request.properties = properties
+        let response = try await request.response()
+        return response.items
     }
 }
