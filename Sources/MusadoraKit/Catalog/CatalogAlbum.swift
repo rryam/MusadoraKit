4ab26aee9578f56517b73d53aa063b1570ecaf72
Rudrank Riyam <rudrankriyam@gmail.com>
2022-04-10 16:02:37 +0530

    Add documentation for catalog song and catalog album

--- Sources/MusadoraKit/Catalog/CatalogAlbum.swift
+++ Sources/MusadoraKit/Catalog/CatalogAlbum.swift
@@ -7,9 +7,15 @@
 
 import MusicKit
 
+/// A collection of albums.
 public typealias Albums = MusicItemCollection<Album>
 
 public extension MusadoraKit {
+    /// Fetch an album from the Apple Music catalog by using its identifier.
+    /// - Parameters:
+    ///   - id: The unique identifier for the album.
+    ///   - properties: Additional relationships to fetch with the album.
+    /// - Returns: `Album` matching the given identifier.
     static func catalogAlbum(id: MusicItemID,
                              with properties: [PartialMusicAsyncProperty<Album>] = []) async throws -> Album {
         var request = MusicCatalogResourceRequest<Album>(matching: \.id, equalTo: id)
@@ -23,22 +29,30 @@ public extension MusadoraKit {
 
         return album
     }
-    
+
+    /// Fetch one or more albums from the Apple Music catalog by using their identifiers.
+    /// - Parameters:
+    ///   - ids: The unique identifiers for the albums.
+    ///   - properties: Additional relationships to fetch with the albums.
+    /// - Returns: `Albums` matching the given identifiers.
     static func catalogAlbums(ids: [MusicItemID],
-                              limit: Int? = nil,
                               with properties: [PartialMusicAsyncProperty<Album>] = []) async throws -> Albums {
         var request = MusicCatalogResourceRequest<Album>(matching: \.id, memberOf: ids)
-        request.limit = limit
         request.properties = properties
 
         let response = try await request.response()
         
         return response.items
     }
-    
-    static func catalogAlbums(isrc: String,
+
+    /// Fetch an album from Apple Music catalog by using their UPC value.
+    /// - Parameters:
+    ///   - upc: The UPC (Universal Product Code) value for the album or single.
+    ///   - properties: Additional relationships to fetch with the album.
+    /// - Returns: `Album` matching the given UPC value.
+    static func catalogAlbums(upc: String,
                               with properties: [PartialMusicAsyncProperty<Album>] = []) async throws -> Album {
-        var request = MusicCatalogResourceRequest<Album>(matching: \.upc, equalTo: isrc)
+        var request = MusicCatalogResourceRequest<Album>(matching: \.upc, equalTo: upc)
         request.properties = properties
 
         let response = try await request.response()
@@ -49,12 +63,15 @@ public extension MusadoraKit {
 
         return album
     }
-    
-    static func catalogAlbums(isrc: [String],
-                              limit: Int? = nil,
+
+    /// Fetch multiple albums from Apple Music catalog by using their UPC values.
+    /// - Parameters:
+    ///   - upc: The UPC (Universal Product Code) values for the albums or singles.
+    ///   - properties: Additional relationships to fetch with the albums.
+    /// - Returns: `Albums` matching the given UPC values.
+    static func catalogAlbums(upc: [String],
                               with properties: [PartialMusicAsyncProperty<Album>] = []) async throws -> Albums {
-        var request = MusicCatalogResourceRequest<Album>(matching: \.upc, memberOf: isrc)
-        request.limit = limit
+        var request = MusicCatalogResourceRequest<Album>(matching: \.upc, memberOf: upc)
         request.properties = properties
 
         let response = try await request.response()
