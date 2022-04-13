6e74bfd1fba86dc1e591aaa9aef6509e71525c71
Rudrank Riyam <rudrankriyam@gmail.com>
2022-04-13 19:10:37 +0530

    Refactor MusicRecommendationRequest

--- Sources/MusadoraKit/Recommendations/MusicRecommendationRequest.swift
+++ Sources/MusadoraKit/Recommendations/MusicRecommendationRequest.swift
@@ -18,27 +18,19 @@ public struct MusicRecommendationRequest {
     public var limit: Int?
 
     /// Creates a request to fetch default recommendations.
-    public init() {
-    }
-
-    /// Creates a request to fetch a recommendation by using its identifier.
-    public init(id: String) {
-        self.id = id
-    }
+    public init() {}
 
     /// Creates a request to fetch one or more recommendations by using their identifiers.
     public init(ids: [String]) {
         self.ids = ids
     }
 
-    private var id: String?
     private var ids: [String]?
 
     /// Fetches recommendations based on the user’s library
     /// and purchase history for the given request.
     public func response() async throws -> MusicRecommendationResponse {
-        guard let url = recommendationEndpointURL else { throw URLError(.badURL) }
-
+        let url = try recommendationEndpointURL
         let request = MusicDataRequest(urlRequest: .init(url: url))
         let response = try await request.response()
         let items = try JSONDecoder().decode(Recommendations.self, from: response.data)
@@ -49,28 +41,28 @@ public struct MusicRecommendationRequest {
 
 @available(iOS 15.0, macOS 12.0, tvOS 15.0, watchOS 8.0, *)
 extension MusicRecommendationRequest {
-    private var recommendationEndpointURL: URL? {
-        var components = URLComponents()
-        components.scheme = "https"
-        components.host = "api.music.apple.com"
-        components.path = "/v1/me/recommendations"
+    private var recommendationEndpointURL: URL {
+        get throws {
+            var components = URLComponents()
+            var queryItems: [URLQueryItem]?
 
-        var queryItems: [URLQueryItem] = []
-
-        if let id = id {
-            components.path += "/\(id)"
-        } else if let ids = ids {
-            queryItems.append(URLQueryItem(name: "ids", value: ids.joined(separator: ",")))
-        } else {
-            if let limit = limit {
-                queryItems.append(URLQueryItem(name: "limit", value: "\(limit)"))
+            components.scheme = "https"
+            components.host = "api.music.apple.com"
+            components.path = "/v1/me/recommendations"
+            
+            if let ids = ids {
+                queryItems = [URLQueryItem(name: "ids", value: ids.joined(separator: ","))]
+            } else if let limit = limit {
+                queryItems = [URLQueryItem(name: "limit", value: "\(limit)")]
             }
-        }
 
-        if !queryItems.isEmpty {
             components.queryItems = queryItems
-        }
 
-        return components.url
+            guard let url = components.url else {
+                throw URLError(.badURL)
+            }
+
+            return url
+        }
     }
 }
