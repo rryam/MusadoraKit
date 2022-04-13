0d262f541f38d85da0aef4f22ffd10b351479c67
Rudrank Riyam <rudrankriyam@gmail.com>
2022-04-13 19:15:22 +0530

    Refactor MusicHistoryRequest and MusicHistoryResponse

--- Sources/MusadoraKit/Recommendations/MusicRecommendationResponse.swift
+++ Sources/MusadoraKit/Recommendations/MusicRecommendationResponse.swift
@@ -16,22 +16,9 @@ public struct MusicRecommendationResponse {
 }
 
 @available(iOS 15.0, macOS 12.0, tvOS 15.0, watchOS 8.0, *)
-extension MusicRecommendationResponse: Equatable {
-    public static func == (lhs: MusicRecommendationResponse, rhs: MusicRecommendationResponse) -> Bool {
-        lhs.items == rhs.items
-    }
-}
+extension MusicRecommendationResponse: Equatable, Hashable, Codable {}
 
 @available(iOS 15.0, macOS 12.0, tvOS 15.0, watchOS 8.0, *)
-extension MusicRecommendationResponse: Hashable {
-    public func hash(into hasher: inout Hasher) {
-        hasher.combine(items)
-    }
-}
-
-@available(iOS 15.0, macOS 12.0, tvOS 15.0, watchOS 8.0, *)
-extension MusicRecommendationResponse: Codable {}
-
 extension MusicRecommendationResponse: CustomStringConvertible {
     public var description: String {
         "MusicRecommendationResponse(\(items.description))"
