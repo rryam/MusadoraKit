359a5c0dd98c15821b868bc71ada5c9d4fa7d033
Rudrank Riyam <rudrankriyam@gmail.com>
2022-04-13 15:13:40 +0530

    Update MusicRecommendationResponse

--- Sources/MusadoraKit/Recommendations/MusicRecommendationResponse.swift
+++ Sources/MusadoraKit/Recommendations/MusicRecommendationResponse.swift
@@ -14,3 +14,26 @@ public struct MusicRecommendationResponse {
     /// A collection of recommendation based on the `MusicRecommendationRequest`.
     public let items: Recommendations
 }
+
+@available(iOS 15.0, macOS 12.0, tvOS 15.0, watchOS 8.0, *)
+extension MusicRecommendationResponse: Equatable {
+    public static func == (lhs: MusicRecommendationResponse, rhs: MusicRecommendationResponse) -> Bool {
+        lhs.items == rhs.items
+    }
+}
+
+@available(iOS 15.0, macOS 12.0, tvOS 15.0, watchOS 8.0, *)
+extension MusicRecommendationResponse: Hashable {
+    public func hash(into hasher: inout Hasher) {
+        hasher.combine(items)
+    }
+}
+
+@available(iOS 15.0, macOS 12.0, tvOS 15.0, watchOS 8.0, *)
+extension MusicRecommendationResponse: Codable {}
+
+extension MusicRecommendationResponse: CustomStringConvertible {
+    public var description: String {
+        "MusicRecommendationResponse(\(items.description))"
+    }
+}
