a69ab2cf163dcc3392e0285575b4bb56ab81f1cc
Rudrank Riyam <rudrankriyam@gmail.com>
2022-04-19 02:33:06 +0530

    Add MusadoraLabsKit struct

--- Sources/MusadoraLabsKit/Apple Music Endpoint/AlbumsEndpoint.swift
+++ Sources/MusadoraLabsKit/Apple Music Endpoint/AlbumsEndpoint.swift
@@ -7,33 +7,33 @@
 
 import Foundation
 
-extension AppleMusicEndpoint {
+extension MusadoraLabsKit {
     // MARK: - Catalog Albums
     static func catalogAlbum(id: String, storeFront: String? = nil) async throws -> URL {
-        try await AppleMusicEndpoint(library: .catalog, path: MusicItemPath.albums.id(id), storeFront: storeFront).url
+        try await MusadoraLabsKit(library: .catalog, path: MusicItemPath.albums.id(id), storeFront: storeFront).url
     }
 
     static func catalogAlbums(ids: [String], storeFront: String? = nil) async throws -> URL {
         let query = URLQueryItem(name: "ids", value: ids.joined(separator: ","))
-        return try await AppleMusicEndpoint(library: .catalog, path: .albums, storeFront: storeFront, queryItems: [query]).url
+        return try await MusadoraLabsKit(library: .catalog, path: .albums, storeFront: storeFront, queryItems: [query]).url
     }
 
     static func catalogAlbums(upcs: [String], storeFront: String? = nil) async throws -> URL {
         let query = URLQueryItem(name: "filter[upc]", value: upcs.joined(separator: ","))
-        return try await AppleMusicEndpoint(library: .catalog, path: .albums, storeFront: storeFront, queryItems: [query]).url
+        return try await MusadoraLabsKit(library: .catalog, path: .albums, storeFront: storeFront, queryItems: [query]).url
     }
 
     // MARK: - Library Albums
     static func libraryAlbum(id: String) async throws -> URL {
-        try await AppleMusicEndpoint(library: .library, path: MusicItemPath.albums.id(id)).url
+        try await MusadoraLabsKit(library: .library, path: MusicItemPath.albums.id(id)).url
     }
 
     static func libraryAlbums(ids: [String]) async throws -> URL {
         let query = URLQueryItem(name: "ids", value: ids.joined(separator: ","))
-        return try await AppleMusicEndpoint(library: .library, path: .albums, queryItems: [query]).url
+        return try await MusadoraLabsKit(library: .library, path: .albums, queryItems: [query]).url
     }
 
     static func libraryAlbums() async throws -> URL {
-        try await AppleMusicEndpoint(library: .library, path: .albums).url
+        try await MusadoraLabsKit(library: .library, path: .albums).url
     }
 }
