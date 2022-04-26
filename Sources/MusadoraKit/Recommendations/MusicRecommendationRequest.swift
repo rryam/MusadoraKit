e21d5fa27b78b9ecc0633970da74fee783b6bf56
Rudrank Riyam <rudrankriyam@gmail.com>
2022-04-27 03:23:08 +0530

    Add MusicRecommendationRequestTests

--- Sources/MusadoraKit/Recommendations/MusicRecommendationRequest.swift
+++ Sources/MusadoraKit/Recommendations/MusicRecommendationRequest.swift
@@ -56,7 +56,7 @@ public struct MusicRecommendationRequest {
 }
 
 extension MusicRecommendationRequest {
-    private var recommendationEndpointURL: URL {
+  internal var recommendationEndpointURL: URL {
         get throws {
             var components = URLComponents()
             var queryItems: [URLQueryItem]?
--- Tests/MusadoraKitTests/MusadoraKitTests.swift
+++ Tests/MusadoraKitTests/MusadoraKitTests.swift
@@ -10,3 +10,8 @@ import XCTest
 
 final class MusadoraKitTests: XCTestCase {
 }
+
+public func XCTAssertEqualEndpoint(_ endpoint: URL, _ url: String) {
+    let url = URL(string: url)!
+    XCTAssertEqual(endpoint, url)
+}
