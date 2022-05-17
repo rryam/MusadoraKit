304ebec1a6e65bc7626fac927f8380e5579687be
Rudrank Riyam <rudrankriyam@gmail.com>
2022-05-18 05:27:58 +0530

    Fix LibraryCatalog

--- Sources/MusadoraKit/Library/LibraryCatalog.swift
+++ Sources/MusadoraKit/Library/LibraryCatalog.swift
@@ -13,31 +13,32 @@ extension FilterableLibraryItem {
         get async throws {
             var components = URLComponents()
             let path: LibraryMusicItemType
-
+            
             components.scheme = "https"
             components.host = "api.music.apple.com"
-
+            components.path = "/v1/me/library/"
+            
             switch self {
                 case is Song: path = .songs
                 case is Album: path = .albums
-                case is Artist.Type: path = .artists
+                case is Artist: path = .artists
                 case is MusicVideo: path = .musicVideos
                 case is Playlist: path = .playlists
                 default: throw NSError(domain: "Wrong library music item type.", code: 0)
             }
-
-            components.path = "/v1/me/library/\(path.rawValue)/\(self.id.rawValue)/catalog"
-
+            
+            components.path += "\(path.rawValue)/\(id.rawValue)/catalog"
+            
             guard let url = components.url else {
                 throw URLError(.badURL)
             }
-
+            
             let request = MusicDataRequest(urlRequest: .init(url: url))
             let response = try await request.response()
             let items = try JSONDecoder().decode(MusicItemCollection<Self>.self, from: response.data)
-
+            
             guard let item = items.first else {
-                throw MusadoraKitError.notFound(for: self.id.rawValue)
+                throw MusadoraKitError.notFound(for: id.rawValue)
             }
             return item
         }
