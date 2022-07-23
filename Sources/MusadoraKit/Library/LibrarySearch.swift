b6cbf513c04600b60e9cad249dad388b5d5a5db7
Rudrank Riyam <rudrankriyam@gmail.com>
2022-07-23 07:07:25 +0530

    Add library search for iOS 16+

--- Sources/MusadoraKit/Library/LibrarySearch.swift
+++ Sources/MusadoraKit/Library/LibrarySearch.swift
@@ -7,6 +7,48 @@
 
 import MusicKit
 
+public enum MusicLibrarySearchType {
+  case songs
+  case albums
+  case playlists
+  case artists
+  case musicVideos
+
+#if compiler(>=5.7)
+  @available(iOS 16.0, tvOS 16.0, watchOS 9.0, *)
+  @available(macOS, unavailable)
+  @available(macCatalyst, unavailable)
+  public var libraryType: MusicLibrarySearchable.Type {
+    switch self {
+      case .songs:
+        return Song.self
+      case .albums:
+        return Album.self
+      case .playlists:
+        return Playlist.self
+      case .artists:
+        return Artist.self
+      case .musicVideos:
+        return MusicVideo.self
+    }
+  }
+#endif
+
+  public var type: MusadoraLibrarySearchable.Type {
+    switch self {
+      case .songs:
+        return Song.self
+      case .albums:
+        return Album.self
+      case .playlists:
+        return Playlist.self
+      case .artists:
+        return Artist.self
+      case .musicVideos:
+        return MusicVideo.self
+    }
+  }
+}
 
 public extension MusadoraKit {
   /// Search the user's library by using a query.
@@ -18,27 +60,33 @@ public extension MusadoraKit {
   /// - Returns: `MusicLibrarySearchResponse` that contains results for a library search request.
   static func librarySearch(
     for term: String,
-    types: [MusadoraLibrarySearchable.Type],
+    types: [MusicLibrarySearchType],
     limit: Int? = nil,
     offset: Int? = nil) async throws -> MusadoraLibrarySearchResponse {
-      var request = MusadoraLibrarySearchRequest(term: term, types: types)
+      let searchTypes = types.map { $0.type }
+      var request = MusadoraLibrarySearchRequest(term: term, types: searchTypes)
       request.limit = limit
       request.offset = offset
       return try await request.response()
     }
+}
 
 #if compiler(>=5.7)
+public extension MusadoraKit {
   @available(iOS 16.0, tvOS 16.0, watchOS 9.0, *)
   @available(macOS, unavailable)
   @available(macCatalyst, unavailable)
   static func librarySearch(
     for term: String,
-    types: [MusicLibrarySearchable.Type],
+    types: [MusicLibrarySearchType],
     limit: Int = 5,
-    offset: Int? = nil) async throws -> MusicLibrarySearchResponse {
-      var request = MusicLibrarySearchRequest(term: term, types: types)
+    offset: Int? = nil,
+    includeTopResults: Bool = false) async throws -> MusicLibrarySearchResponse {
+      let searchTypes = types.map { $0.libraryType }
+      var request = MusicLibrarySearchRequest(term: term, types: searchTypes)
       request.limit = limit
+      request.includeTopResults = includeTopResults
       return try await request.response()
     }
-#endif
 }
+#endif
