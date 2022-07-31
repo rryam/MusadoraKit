6e684272cce1c991a70c63639195ba5922e55c73
Rudrank Riyam <rudrankriyam@gmail.com>
2022-08-01 03:52:12 +0530

    Fix tests of MusadoraLabsKit

--- Sources/MusadoraLabsKit/Apple Music Endpoint/AppleMusicEndpoint.swift
+++ Sources/MusadoraLabsKit/Apple Music Endpoint/AppleMusicEndpoint.swift
@@ -9,10 +9,10 @@ import Foundation
 import MusicKit
 
 extension MusadoraLabsKit {
-  var url: URL {
+  internal var url: URL {
     get async throws {
       var components = URLComponents()
-      
+      components.scheme = "https"
       components.host = "api.music.apple.com"
       components.path = library.path
 
@@ -21,14 +21,14 @@ extension MusadoraLabsKit {
       }
 
       switch library {
-      case .catalog:
-        if let storeFront = storeFront {
-          components.path += storeFront + "/" + path
-        } else {
-          let storeFront = try await MusicDataRequest.currentCountryCode
-          components.path += storeFront + "/" + path
-        }
-      case .user, .library: components.path += path
+        case .catalog:
+          if let storeFront = storeFront {
+            components.path += storeFront + "/" + path
+          } else {
+            let storeFront = try await MusicDataRequest.currentCountryCode
+            components.path += storeFront + "/" + path
+          }
+        case .user, .library: components.path += path
       }
 
       guard let url = components.url else {
