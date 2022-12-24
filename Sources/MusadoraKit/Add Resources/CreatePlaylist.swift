77b954e05b5f29dffba2142dad7b691aa8ad6983
Brett Bauman <bauman.brett3@gmail.com>
2022-12-23 19:59:12 -0700

    Create an endpoint to add songs to a playlist

--- Sources/MusadoraKit/Add Resources/CreatePlaylist.swift
+++ Sources/MusadoraKit/Add Resources/CreatePlaylist.swift
@@ -104,6 +104,20 @@ public extension MusadoraKit {
 
   }
 
+  static func addSongs(_ songs: Songs, toPlaylist id: MusicItemID) async throws {
+    let songData = songs.map { PlaylistCreationData(id: $0.id.rawValue, type: .song) }
+    let tracks = PlaylistCreationTracks(data: songData)
+
+    let url = URL(string: "https://api.music.apple.com/v1/me/library/playlists/\(id.rawValue)/tracks")
+
+    guard let url = url else { throw URLError(.badURL) }
+
+    let data = try JSONEncoder().encode(tracks)
+
+    let request = MusicDataPostRequest(url: url, data: data)
+    let _ = try await request.response()
+  }
+
   static private func createPlaylist(with creationRequest: LibraryPlaylistCreationRequest) async throws -> Playlist {
     let url = URL(string: "https://api.music.apple.com/v1/me/library/playlists")
 
