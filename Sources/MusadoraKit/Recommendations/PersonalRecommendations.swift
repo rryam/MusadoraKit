f040e068199fad5c84efa4851373a21fec7521fe
Rudrank Riyam <rudrankriyam@gmail.com>
2022-12-25 17:51:15 +0530

    Fix for recommendations on Xcode 13.4.1

--- Sources/MusadoraKit/Recommendations/PersonalRecommendations.swift
+++ Sources/MusadoraKit/Recommendations/PersonalRecommendations.swift
@@ -7,6 +7,8 @@
 
 import MusicKit
 
+#if compiler(>=5.7)
 /// A collection of personal recommendations.
 @available(iOS 16.0, macOS 13.0, tvOS 16.0, watchOS 9.0, *)
 public typealias PersonalRecommendations = MusicItemCollection<MusicPersonalRecommendation>
+#endif
