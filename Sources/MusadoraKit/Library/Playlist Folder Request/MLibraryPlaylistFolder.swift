dd5c1736b24f6ecd72f6c37cdac6751d2eeb4cdc
Rudrank Riyam <rudrankriyam@gmail.com>
2023-01-04 11:34:09 +0530

    add libraryplaylistfolder

---
+++ Sources/MusadoraKit/Library/Playlist Folder Request/MLibraryPlaylistFolder.swift
@@ -0,0 +1,16 @@
+//
+//  MLibraryPlaylistFolder.swift
+//  
+//
+//  Created by Rudrank Riyam on 26/12/22.
+//
+
+import MusicKit
+
+public typealias LibraryPlaylistFolders = [LibraryPlaylistFolder]
+
+extension MLibrary {
+//  static func rootPlaylistsFolder() async throws -> LibraryPlaylistFolders {
+//
+//  }
+}
