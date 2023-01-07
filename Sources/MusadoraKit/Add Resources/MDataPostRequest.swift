56396097a11b433a06a94d24cb675ada8980b789
Rudrank Riyam <rudrankriyam@gmail.com>
2023-01-07 23:15:50 +0530

    Update syntax for catalog songs and make MDataPostRequest public

--- Sources/MusadoraKit/Add Resources/MDataPostRequest.swift
+++ Sources/MusadoraKit/Add Resources/MDataPostRequest.swift
@@ -9,23 +9,23 @@ import Foundation
 import MusicKit
 
 /// A request for uploading data from an arbitrary Apple Music API endpoint.
-struct MDataPostRequest {
+public struct MDataPostRequest {
 
   /// The URL for the data request.
-  var url: URL
+  private var url: URL
 
   /// Data to encode for the POST request.
-  var data: Data?
+  private var data: Data?
 
   /// Creates a data request with the given URL.
-  init(url: URL, data: Data? = nil) {
+  public init(url: URL, data: Data? = nil) {
     self.url = url
     self.data = data
   }
 
   /// Uploads data the Apple Music API endpoint that
   /// the URL request defines.
-  func response() async throws -> MusicDataResponse {
+  public func response() async throws -> MusicDataResponse {
     var urlRequest = URLRequest(url: url)
     urlRequest.httpMethod = "POST"
     urlRequest.httpBody = data
