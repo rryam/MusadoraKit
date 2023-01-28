12c6abdb2b28a8317e4fcde31c6fc4a4815395fc
Rudrank Riyam <rudrankriyam@gmail.com>
2023-01-28 18:07:47 +0530

    Fix create playlist for Xcode 13

--- Sources/MusadoraKit/Add Resources/CreatePlaylist.swift
+++ Sources/MusadoraKit/Add Resources/CreatePlaylist.swift
@@ -10,6 +10,7 @@ import MusicKit
 
 public extension MLibrary {
 
+#if compiler(>=5.7)
   /// Creates a playlist in the user’s music library.
   ///
   /// - Parameters:
@@ -40,144 +41,145 @@ public extension MLibrary {
   static func createPlaylist(with name: String, author: String? = nil, description: String? = nil, items: Songs) async throws -> Playlist {
     try await MusicLibrary.shared.createPlaylist(name: name, description: description, authorDisplayName: author, items: items)
   }
+#endif
 
-	/// Creates a playlist in the user’s music library.
-	///
-	/// - Parameters:
-	///   - name: The name of the playlist.
-	///   - description: An optional description of the playlist.
-	/// - Returns: The newly created playlist.
-	static func createPlaylist(with name: String, description: String? = nil) async throws -> Playlist {
-		let creationRequest = LibraryPlaylistCreationRequest(attributes: .init(name: name, description: description))
-		return try await createPlaylist(with: creationRequest)
-	}
+  /// Creates a playlist in the user’s music library.
+  ///
+  /// - Parameters:
+  ///   - name: The name of the playlist.
+  ///   - description: An optional description of the playlist.
+  /// - Returns: The newly created playlist.
+  static func createPlaylist(with name: String, description: String? = nil) async throws -> Playlist {
+    let creationRequest = LibraryPlaylistCreationRequest(attributes: .init(name: name, description: description))
+    return try await createPlaylist(with: creationRequest)
+  }
 
-	/// Creates a playlist in the user’s music library with songs.
-	///
-	/// - Parameters:
-	///   - name: The name of the playlist.
-	///   - description: An optional description of the playlist.
-	///   - items: Add the songs to the playlist.
-	/// - Returns: The newly created playlist.
-	static func createPlaylist(with name: String, description: String? = nil, items: Songs) async throws -> Playlist {
-		let tracksData: [PlaylistCreationData] = items.map { .init(id: $0.id.rawValue, type: .song)}
+  /// Creates a playlist in the user’s music library with songs.
+  ///
+  /// - Parameters:
+  ///   - name: The name of the playlist.
+  ///   - description: An optional description of the playlist.
+  ///   - items: Add the songs to the playlist.
+  /// - Returns: The newly created playlist.
+  static func createPlaylist(with name: String, description: String? = nil, items: Songs) async throws -> Playlist {
+    let tracksData: [PlaylistCreationData] = items.map { .init(id: $0.id.rawValue, type: .song)}
 
-		let creationRequest = LibraryPlaylistCreationRequest(attributes: .init(name: name, description: description), relationships: .init(tracks: .init(data: tracksData)))
-		return try await createPlaylist(with: creationRequest)
-	}
+    let creationRequest = LibraryPlaylistCreationRequest(attributes: .init(name: name, description: description), relationships: .init(tracks: .init(data: tracksData)))
+    return try await createPlaylist(with: creationRequest)
+  }
 
-	/// Creates a playlist in the user’s music library with song IDs.
-	///
-	/// - Parameters:
-	///   - name: The name of the playlist.
-	///   - description: An optional description of the playlist.
-	///   - songIds: IDs of Songs
-	/// - Returns: The newly created playlist.
-	static func createPlaylist(with name: String, description: String? = nil, songIds: [MusicItemID]) async throws -> Playlist {
-		let tracksData: [PlaylistCreationData] = songIds.map { .init(id: $0.rawValue, type: .song) }
+  /// Creates a playlist in the user’s music library with song IDs.
+  ///
+  /// - Parameters:
+  ///   - name: The name of the playlist.
+  ///   - description: An optional description of the playlist.
+  ///   - songIds: IDs of Songs
+  /// - Returns: The newly created playlist.
+  static func createPlaylist(with name: String, description: String? = nil, songIds: [MusicItemID]) async throws -> Playlist {
+    let tracksData: [PlaylistCreationData] = songIds.map { .init(id: $0.rawValue, type: .song) }
 
-		let creationRequest = LibraryPlaylistCreationRequest(attributes: .init(name: name, description: description), relationships: .init(tracks: .init(data: tracksData)))
-		return try await createPlaylist(with: creationRequest)
-	}
+    let creationRequest = LibraryPlaylistCreationRequest(attributes: .init(name: name, description: description), relationships: .init(tracks: .init(data: tracksData)))
+    return try await createPlaylist(with: creationRequest)
+  }
 
-	static func createPlaylist(with name: String, description: String? = nil, items: Tracks) async throws -> Playlist {
-		let tracksData: [PlaylistCreationData] = try items.compactMap {
-			let data = try JSONEncoder().encode($0.playParameters)
-			let playParameters = try JSONDecoder().decode(MusicPlayParameters.self, from: data)
-			let id = playParameters.id.rawValue
+  static func createPlaylist(with name: String, description: String? = nil, items: Tracks) async throws -> Playlist {
+    let tracksData: [PlaylistCreationData] = try items.compactMap {
+      let data = try JSONEncoder().encode($0.playParameters)
+      let playParameters = try JSONDecoder().decode(MusicPlayParameters.self, from: data)
+      let id = playParameters.id.rawValue
 
-			if playParameters.kind == "song" {
-				if let isLibrary = playParameters.isLibrary, isLibrary {
-					return PlaylistCreationData(id: id, type: .librarySong)
-				} else {
-					return PlaylistCreationData(id: id, type: .song)
-				}
-			} else if playParameters.kind == "musicVideo" {
-				if let isLibrary = playParameters.isLibrary, isLibrary {
-					return PlaylistCreationData(id: id, type: .libraryMusicVideo)
-				} else {
-					return PlaylistCreationData(id: id, type: .musicVideo)
-				}
-			} else {
-				return nil
-			}
-		}
+      if playParameters.kind == "song" {
+        if let isLibrary = playParameters.isLibrary, isLibrary {
+          return PlaylistCreationData(id: id, type: .librarySong)
+        } else {
+          return PlaylistCreationData(id: id, type: .song)
+        }
+      } else if playParameters.kind == "musicVideo" {
+        if let isLibrary = playParameters.isLibrary, isLibrary {
+          return PlaylistCreationData(id: id, type: .libraryMusicVideo)
+        } else {
+          return PlaylistCreationData(id: id, type: .musicVideo)
+        }
+      } else {
+        return nil
+      }
+    }
 
-		let creationRequest = LibraryPlaylistCreationRequest(attributes: .init(name: name, description: description), relationships: .init(tracks: .init(data: tracksData)))
-		return try await createPlaylist(with: creationRequest)
-	}
+    let creationRequest = LibraryPlaylistCreationRequest(attributes: .init(name: name, description: description), relationships: .init(tracks: .init(data: tracksData)))
+    return try await createPlaylist(with: creationRequest)
+  }
 
-	static func createPlaylist(with name: String, description: String? = nil, items: [any PlaylistAddable]) async throws -> Playlist {
-		let tracksData: [PlaylistCreationData] = try items.compactMap {
-			let data = try JSONEncoder().encode($0.playParameters)
-			let playParameters = try JSONDecoder().decode(MusicPlayParameters.self, from: data)
-			let id = playParameters.id.rawValue
+  static func createPlaylist(with name: String, description: String? = nil, items: [any PlaylistAddable]) async throws -> Playlist {
+    let tracksData: [PlaylistCreationData] = try items.compactMap {
+      let data = try JSONEncoder().encode($0.playParameters)
+      let playParameters = try JSONDecoder().decode(MusicPlayParameters.self, from: data)
+      let id = playParameters.id.rawValue
 
-			if playParameters.kind == "song" {
-				if let isLibrary = playParameters.isLibrary, isLibrary {
-					return PlaylistCreationData(id: id, type: .librarySong)
-				} else {
-					return PlaylistCreationData(id: id, type: .song)
-				}
-			} else if playParameters.kind == "musicVideo" {
-				if let isLibrary = playParameters.isLibrary, isLibrary {
-					return PlaylistCreationData(id: id, type: .libraryMusicVideo)
-				} else {
-					return PlaylistCreationData(id: id, type: .musicVideo)
-				}
-			} else {
-				return nil
-			}
-		}
+      if playParameters.kind == "song" {
+        if let isLibrary = playParameters.isLibrary, isLibrary {
+          return PlaylistCreationData(id: id, type: .librarySong)
+        } else {
+          return PlaylistCreationData(id: id, type: .song)
+        }
+      } else if playParameters.kind == "musicVideo" {
+        if let isLibrary = playParameters.isLibrary, isLibrary {
+          return PlaylistCreationData(id: id, type: .libraryMusicVideo)
+        } else {
+          return PlaylistCreationData(id: id, type: .musicVideo)
+        }
+      } else {
+        return nil
+      }
+    }
 
-		let creationRequest = LibraryPlaylistCreationRequest(attributes: .init(name: name, description: description), relationships: .init(tracks: .init(data: tracksData)))
-		return try await createPlaylist(with: creationRequest)
+    let creationRequest = LibraryPlaylistCreationRequest(attributes: .init(name: name, description: description), relationships: .init(tracks: .init(data: tracksData)))
+    return try await createPlaylist(with: creationRequest)
 
-	}
+  }
 
-	@discardableResult static func add(songIDs: [MusicItemID], to playlistID: MusicItemID) async throws -> Bool {
-		let songData = songIDs.map { PlaylistCreationData(id: $0.rawValue, type: .song) }
-		let tracks = PlaylistCreationTracks(data: songData)
+  @discardableResult static func add(songIDs: [MusicItemID], to playlistID: MusicItemID) async throws -> Bool {
+    let songData = songIDs.map { PlaylistCreationData(id: $0.rawValue, type: .song) }
+    let tracks = PlaylistCreationTracks(data: songData)
 
-		let url = URL(string: "https://api.music.apple.com/v1/me/library/playlists/\(playlistID.rawValue)/tracks")
+    let url = URL(string: "https://api.music.apple.com/v1/me/library/playlists/\(playlistID.rawValue)/tracks")
 
-		guard let url = url else { throw URLError(.badURL) }
+    guard let url = url else { throw URLError(.badURL) }
 
-		let data = try JSONEncoder().encode(tracks)
+    let data = try JSONEncoder().encode(tracks)
 
-		let request = MDataPostRequest(url: url, data: data)
-		let response = try await request.response()
-		return response.urlResponse.statusCode == 201
-	}
+    let request = MDataPostRequest(url: url, data: data)
+    let response = try await request.response()
+    return response.urlResponse.statusCode == 201
+  }
 
-	@discardableResult static func add(songs: Songs, to playlist: Playlist) async throws -> Bool {
-		try await add(songIDs: songs.map(\.id), to: playlist.id)
-	}
+  @discardableResult static func add(songs: Songs, to playlist: Playlist) async throws -> Bool {
+    try await add(songIDs: songs.map(\.id), to: playlist.id)
+  }
 
-	static private func createPlaylist(with creationRequest: LibraryPlaylistCreationRequest) async throws -> Playlist {
-		let url = URL(string: "https://api.music.apple.com/v1/me/library/playlists")
+  static private func createPlaylist(with creationRequest: LibraryPlaylistCreationRequest) async throws -> Playlist {
+    let url = URL(string: "https://api.music.apple.com/v1/me/library/playlists")
 
-		guard let url = url else { throw URLError(.badURL) }
+    guard let url = url else { throw URLError(.badURL) }
 
-		let data = try JSONEncoder().encode(creationRequest)
+    let data = try JSONEncoder().encode(creationRequest)
 
-		let request = MDataPostRequest(url: url, data: data)
-		let response = try await request.response()
+    let request = MDataPostRequest(url: url, data: data)
+    let response = try await request.response()
 
-		let playlists = try JSONDecoder().decode(Playlists.self, from: response.data)
+    let playlists = try JSONDecoder().decode(Playlists.self, from: response.data)
 
-		guard let playlist = playlists.first else {
-			throw MusadoraKitError.notFound(for: creationRequest.attributes.name)
-		}
+    guard let playlist = playlists.first else {
+      throw MusadoraKitError.notFound(for: creationRequest.attributes.name)
+    }
 
-		return playlist
-	}
+    return playlist
+  }
 }
 
 public protocol PlaylistAddable: MusicItem {
 
-	/// The parameters to use to play the item.
-	var playParameters: PlayParameters? { get }
+  /// The parameters to use to play the item.
+  var playParameters: PlayParameters? { get }
 }
 
 extension Song: PlaylistAddable { }
@@ -187,50 +189,50 @@ extension Track: PlaylistAddable { }
 extension MusicVideo: PlaylistAddable { }
 
 public struct MusicPlayParameters: Codable {
-	public var id: MusicItemID
-	public var isLibrary: Bool?
-	public var kind: String
-	public var catalogId: MusicItemID?
-	public var globalId: MusicItemID?
+  public var id: MusicItemID
+  public var isLibrary: Bool?
+  public var kind: String
+  public var catalogId: MusicItemID?
+  public var globalId: MusicItemID?
 }
 
 enum LibraryPlaylistCreationTrackType: String, Codable {
-	case song = "songs"
-	case musicVideo = "music-videos"
-	case libraryMusicVideo = "library-music-videos"
-	case librarySong = "library-songs"
+  case song = "songs"
+  case musicVideo = "music-videos"
+  case libraryMusicVideo = "library-music-videos"
+  case librarySong = "library-songs"
 }
 
 struct LibraryPlaylistCreationRequest: Codable {
-	/// A dictionary that includes strings for the name and description of the new playlist.
-	var attributes: PlaylistCreationAttributes
+  /// A dictionary that includes strings for the name and description of the new playlist.
+  var attributes: PlaylistCreationAttributes
 
-	/// To include tracks for the new playlist.
-	var relationships: PlaylistCreationRelationships?
+  /// To include tracks for the new playlist.
+  var relationships: PlaylistCreationRelationships?
 }
 
 struct PlaylistCreationAttributes: Codable {
-	/// The name of the playlist.
-	var name: String
+  /// The name of the playlist.
+  var name: String
 
-	/// The description of the playlist.
-	var description: String?
+  /// The description of the playlist.
+  var description: String?
 }
 
 struct PlaylistCreationRelationships: Codable {
-	var tracks: PlaylistCreationTracks
+  var tracks: PlaylistCreationTracks
 }
 
 struct PlaylistCreationTracks: Codable {
-	/// Data of the tracks to add to the created library playlist.
-	var data: [PlaylistCreationData]
+  /// Data of the tracks to add to the created library playlist.
+  var data: [PlaylistCreationData]
 }
 
 struct PlaylistCreationData: Codable {
-	/// The unique identifier for the track.
-	/// This ID can be a catalog identifier or a library identifier, depending on the track type.
-	var id: String
+  /// The unique identifier for the track.
+  /// This ID can be a catalog identifier or a library identifier, depending on the track type.
+  var id: String
 
-	/// The type of the track to be added.
-	var type: LibraryPlaylistCreationTrackType
+  /// The type of the track to be added.
+  var type: LibraryPlaylistCreationTrackType
 }
