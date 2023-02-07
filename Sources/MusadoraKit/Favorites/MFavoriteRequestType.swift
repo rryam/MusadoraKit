60b01b23eec7b54f45bec5c8479d04caac007971
Rudrank Riyam <rudrankriyam@gmail.com>
2023-02-07 23:02:38 +0530

    Add favorite/remove favorite artist method

---
+++ Sources/MusadoraKit/Favorites/MFavoriteRequestType.swift
@@ -0,0 +1,13 @@
+//
+//  MFavoriteRequestType.swift
+//  MusadoraKit+
+//
+//  Created by Rudrank Riyam on 25/01/23.
+//
+
+import Foundation
+
+enum MFavoriteRequestType {
+  case favorite
+  case removeFavorite
+}
---
+++ Sources/MusadoraKit/Favorites/MFavoritesRequest.swift
@@ -0,0 +1,54 @@
+//
+//  MFavoritesRequest.swift
+//  MusadoraKit+
+//
+//  Created by Rudrank Riyam on 25/01/23.
+//
+
+import Foundation
+import MusicKit
+
+struct MFavoritesRequest {
+  private var artistID: MusicItemID
+  private var type: MFavoriteRequestType
+
+  public init(artist: Artist, type: MFavoriteRequestType) {
+    self.artistID = artist.id
+    self.type = type
+  }
+
+  public func response() async throws -> Bool {
+    switch type {
+      case .favorite:
+        let url = try favortiesEndpointURL
+        let request = MDataPostRequest(url: url)
+        let response = try await request.response()
+        return response.urlResponse.statusCode == 202
+      case .removeFavorite:
+        let url = try favortiesEndpointURL
+        let request = MDataDeleteRequest(url: url)
+        let response = try await request.response()
+        return response.urlResponse.statusCode == 204
+    }
+  }
+}
+
+extension MFavoritesRequest {
+  internal var favortiesEndpointURL: URL {
+    get throws {
+      var components = AppleMusicURLComponents()
+      var queryItems: [URLQueryItem] = []
+      components.path = "me/favorites"
+
+      queryItems.append(URLQueryItem(name: "ids[artists]", value: artistID.rawValue))
+
+      components.queryItems = queryItems
+
+      guard let url = components.url else {
+        throw URLError(.badURL)
+      }
+
+      return url
+    }
+  }
+}
