e4fb238a20f4fd18d55bbb98055e26e622746745
Rudrank Riyam <rudrankriyam@gmail.com>
2023-03-13 01:46:43 +0530

    Add RecommendationsView

---
+++ Musadora/Musadora/History/HistoryView.swift
@@ -0,0 +1,20 @@
+//
+//  HistoryView.swift
+//  Musadora
+//
+//  Created by Rudrank Riyam on 13/03/23.
+//
+
+import SwiftUI
+
+struct HistoryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HistoryView_Previews: PreviewProvider {
+    static var previews: some View {
+        HistoryView()
+    }
+}
