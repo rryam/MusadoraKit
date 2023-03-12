3aeaad9c32b0eda93c54d87ea30a50ab8e56f94f
Rudrank Riyam <rudrankriyam@gmail.com>
2023-03-13 01:59:02 +0530

    Update MusadoraKit in Musadora

--- Musadora/Musadora/History/HistoryView.swift
+++ Musadora/Musadora/History/HistoryView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct HistoryView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("HistoryView")
     }
 }
 
