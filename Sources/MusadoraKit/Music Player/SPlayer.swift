e0b555b868ed2ea56426f9785a7f349e4daae5d6
Rudrank Riyam <rudrankriyam@gmail.com>
2023-03-12 20:06:45 +0530

    Add more methods to APlayer and SPlayer

--- Sources/MusadoraKit/Music Player/SPlayer.swift
+++ Sources/MusadoraKit/Music Player/SPlayer.swift
@@ -11,3 +11,50 @@ import MusicKit
 @available(macOS, unavailable)
 @available(watchOS, unavailable)
 public typealias SPlayer = SystemMusicPlayer
+
+@available(macOS, unavailable)
+@available(watchOS, unavailable)
+public extension SPlayer {
+  func play(song: Song) async throws {
+    queue = [song]
+    try await play()
+  }
+
+  func play(songs: Songs) async throws {
+    queue = SystemMusicPlayer.Queue(for: songs)
+    try await play()
+  }
+
+  func play(song: Song, at position: SPlayer.Queue.EntryInsertionPosition) async throws {
+    try await queue.insert(song, position: position)
+    try await play()
+  }
+}
+
+
+@available(macOS, unavailable)
+@available(watchOS, unavailable)
+public extension SPlayer {
+  func play(station: Station) async throws {
+    queue = [station]
+    try await play()
+  }
+}
+
+@available(macOS, unavailable)
+@available(watchOS, unavailable)
+public extension SPlayer {
+  func play(playlist: Playlist) async throws {
+    queue = [playlist]
+    try await play()
+  }
+}
+
+@available(macOS, unavailable)
+@available(watchOS, unavailable)
+public extension SPlayer {
+  func play(album: Album) async throws {
+    queue = [album]
+    try await play()
+  }
+}
