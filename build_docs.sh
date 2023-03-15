9346942a1d90d0c20775e9fa880bace421914989
Rudrank Riyam <rudrankriyam@gmail.com>
2023-03-16 01:12:35 +0530

    Delete build_docs.sh

--- build_docs.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/zsh
-
-swift package --allow-writing-to-directory ./docs \
-generate-documentation --target MusadoraKit \
---disable-indexing \
---transform-for-static-hosting \
---hosting-base-path MusadoraKit \
---output-path ./docs
