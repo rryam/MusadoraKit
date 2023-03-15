3afc862ddcdbaf141c60f43a75680c227329f6a4
Rudrank Riyam <rudrankriyam@gmail.com>
2023-03-16 00:19:33 +0530

    Update app icon

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-#!/bin/zsh
+#!/bin/bash
 
 declare -a DESTINATIONS=("platform=iOS Simulator,name=iPhone 14" "platform=watchOS Simulator,name=Apple Watch Series 8 (45mm)" "platform=tvOS Simulator,name=Apple TV 4K (3rd generation)" "platform=macOS")
 
