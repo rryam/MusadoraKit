d01656585db553847781ee68f37e63fae0f16ce6
Rudrank Riyam <rudrankriyam@gmail.com>
2023-03-24 04:26:22 +0530

    Add storefront(id:) method

--- Sources/MusadoraKit/Storefronts.swift
+++ Sources/MusadoraKit/Storefronts.swift
@@ -38,14 +38,55 @@ public struct StorefrontsData: Codable {
 }
 
 public extension MCatalog {
+  
+  /// Fetch a specific storefront of Apple Music by its unique identifier.
+  ///
+  /// - Parameter id: The unique identifier for the storefront you want to fetch.
+  /// - Returns: `Storefront` object containing the details of the requested storefront.
+  static func storefront(id: String) async throws -> StorefrontsData.Storefront {
+    let url = try storefrontsURL(id: id)
+
+    let request = MusicDataRequest(urlRequest: .init(url: url))
+    let response = try await request.response()
+    let storefront = try JSONDecoder().decode(StorefrontsData.self, from: response.data).data.first
+
+    guard let storefront else {
+      throw NSError(domain: "Storefront not found for ID \(id).", code: 0)
+    }
+
+    return storefront
+  }
+
+  internal static func storefrontsURL(id: String) throws -> URL {
+    var urlComponents = AppleMusicURLComponents()
+    urlComponents.path = "storefronts/\(id)"
+
+    guard let url = urlComponents.url else {
+      throw URLError(.badURL)
+    }
+
+    return url
+  }
+
   /// Fetch all storefronts of Apple Music.
   ///
   /// - Returns: `Storefronts` as array of storefronts.
   static func storefronts() async throws -> Storefronts {
-    let url = URL(string: "https://api.music.apple.com/v1/storefronts")!
+    let url = try storefrontsURL()
     let request = MusicDataRequest(urlRequest: .init(url: url))
     let response = try await request.response()
     let storefronts = try JSONDecoder().decode(StorefrontsData.self, from: response.data)
     return storefronts.data
+  }
+
+  internal static func storefrontsURL() throws -> URL {
+    var urlComponents = AppleMusicURLComponents()
+    urlComponents.path = "storefronts"
+
+    guard let url = urlComponents.url else {
+      throw URLError(.badURL)
+    }
+
+    return url
   }
 }
