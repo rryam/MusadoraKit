db8ec1fa21351265826834c3ceacbd3b4622e6c2
Rudrank Riyam <rudrankriyam@gmail.com>
2023-04-08 18:07:22 +0530

    Add stationSongs and continuousSongs detailed documentation

--- Sources/MusadoraKit/Continuous/ContinuousSongs.swift
+++ Sources/MusadoraKit/Continuous/ContinuousSongs.swift
@@ -6,12 +6,31 @@
 //
 
 public extension MRecommendation {
-  /// Retrieve recommended songs from Apple Music catalog for a given song, with the option to limit the number of results.
+
+  /// Retrieves recommended songs from the Apple Music catalog for a given song, with the option to limit the number of results.
+  ///
+  /// Example usage:
+  ///
+  ///     do {
+  ///       let song: Song = ...
+  ///       let recommendedSongs = try await MRecommendation.continuousSongs(for: song, limit: 10)
+  ///
+  ///       for recommendedSong in recommendedSongs {
+  ///         print("Recommended song name: \(recommendedSong.title)")
+  ///         print("Recommended song artist: \(recommendedSong.artistName)")
+  ///         // access other recommended song properties as needed
+  ///       }
+  ///     } catch {
+  ///       print("Error retrieving recommended songs: \(error.localizedDescription)")
+  ///     }
   ///
   /// - Parameters:
-  ///    - song: The song for which you want to retrieve recommendations.
-  ///    - limit: Determines the number of recommendations you want to retrieve. The default value is 20.
-  /// - Returns: `Songs` containing the recommended songs.
+  ///   - song: The song for which you want to retrieve recommendations.
+  ///   - limit: Determines the number of recommendations you want to retrieve. The default value is 20.
+  ///
+  /// - Returns: An array of `Song` objects containing the recommended songs.
+  ///
+  /// - Note: This method uses a private endpoint and should be used at your own risk.
   @available(*, message: "THIS IS A PRIVATE ENDPOINT. USE IT AT YOUR OWN RISK.")
   static func continuousSongs(for song: Song, limit: Int = 20) async throws -> Songs {
     var request = MContinuousTrackRequest(for: song)
--- Sources/MusadoraKit/Station Tracks/StationSongs.swift
+++ Sources/MusadoraKit/Station Tracks/StationSongs.swift
@@ -7,14 +7,30 @@
 
 public extension MRecommendation {
 
-  /// Retrieve the station songs from Apple Music catalog for a given station, with the option to limit the number of results.
-  /// This is not possible to do in the official Apple Music app.
+  /// Retrieves station songs from the Apple Music catalog for a given station, with the option to limit the number of results.
+  ///
+  /// Example usage:
+  ///
+  ///     do {
+  ///       let station: Station = ...
+  ///       let stationSongs = try await MRecommendation.stationSongs(for: station, limit: 10)
+  ///
+  ///       for stationSong in stationSongs {
+  ///         print("Station song name: \(stationSong.title)")
+  ///         print("Station song artist: \(stationSong.artistName)")
+  ///         // access other station song properties as needed
+  ///       }
+  ///     } catch {
+  ///       print("Error retrieving station songs: \(error.localizedDescription)")
+  ///     }
   ///
   /// - Parameters:
-  ///    - station: The station for which you want to retrieve the songs.
-  ///    - limit: Determines the number of songs you want to retrieve. The default value is 20.
+  ///   - station: The station for which you want to retrieve the songs. This parameter is of type `Station`, which is a custom type defined in the MusadoraKit library.
+  ///   - limit: Determines the number of songs you want to retrieve. The default value is 20.
   ///
-  /// - Returns: `Songs` containing the station songs.
+  /// - Returns: An array of `Song` objects containing the station songs.
+  ///
+  /// - Note: This method uses a private endpoint and should be used at your own risk. This functionality is not available in the official Apple Music app.
   @available(*, message: "THIS IS A PRIVATE ENDPOINT. USE IT AT YOUR OWN RISK.")
   static func stationSongs(for station: Station, limit: Int = 20) async throws -> Songs {
     var request = MStationTrackRequest(for: station)
