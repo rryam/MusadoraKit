30db4f2b8ef1e7db0d703474970751f79e1a632c
Rudrank Riyam <rudrankriyam@gmail.com>
2023-04-09 01:10:44 +0530

    Update CatalogStation docs

--- Sources/MusadoraKit/Catalog/CatalogStation.swift
+++ Sources/MusadoraKit/Catalog/CatalogStation.swift
@@ -8,9 +8,21 @@
 import Foundation
 
 public extension MCatalog {
+
   /// Fetch a station from the Apple Music catalog by using its identifier.
+  ///
+  /// Example usage:
+  ///
+  ///     do {
+  ///       let station = try await MCatalog.station(id: "12345678")
+  ///       print(station.name)
+  ///     } catch {
+  ///       print("Error fetching station: (error.localizedDescription)")
+  ///     }
+  ///
   /// - Parameters:
   ///   - id: The unique identifier for the station.
+  ///
   /// - Returns: `Station` matching the given identifier.
   static func station(id: MusicItemID) async throws -> Station {
     let request = MusicCatalogResourceRequest<Station>(matching: \.id, equalTo: id)
@@ -23,8 +35,21 @@ public extension MCatalog {
   }
 
   /// Fetch multiple stations from the Apple Music catalog by using their identifiers.
+  ///
+  /// Example usage:
+  ///
+  ///     do {
+  ///       let stationIds = ["12345678", "23456789", "34567890"]
+  ///       let stations = try await MCatalog.stations(ids: stationIds)
+  ///
+  ///       print(stations)
+  ///     } catch {
+  ///       print("Error fetching stations: (error.localizedDescription)")
+  ///     }
+  ///
   /// - Parameters:
   ///   - ids: The unique identifier for the stations.
+  ///
   /// - Returns: `Stations` matching the given identifiers.
   static func stations(ids: [MusicItemID]) async throws -> Stations {
     let request = MusicCatalogResourceRequest<Station>(matching: \.id, memberOf: ids)
@@ -73,9 +98,25 @@ public extension MCatalog {
 
 // MARK: - GENRE STATION
 public extension MCatalog {
+
   /// Fetch a list of stations in the current country's storefront that belong to a specific station genre in the Apple Music catalog.
   ///
-  /// - Parameter genre: The `StationGenre` object representing the genre for which to retrieve the stations.
+  /// Example usage:
+  ///
+  ///     do {
+  ///       let genre = try await MCatalog.genre(id: "34")
+  ///       let stations = try await MCatalog.stations(for: genre)
+  ///
+  ///       for station in stations {
+  ///         print(station.name)
+  ///       }
+  ///     } catch {
+  ///       print("Error fetching stations: (error.localizedDescription)")
+  ///     }
+  ///
+  /// - Parameters:
+  ///   - genre: The `StationGenre` object representing the genre for which to retrieve the stations.
+  ///
   /// - Returns: `Stations` representing the list of stations belonging to the specified genre.
   static func stations(for genre: StationGenre) async throws -> Stations {
     let storefront = try await MusicDataRequest.currentCountryCode
@@ -88,7 +129,19 @@ public extension MCatalog {
     return stations
   }
 
-  /// Fetch a list of stations in the current country's storefront that belongs to all station genres in the Apple Music catalog.
+  /// Fetch a list of stations in the current country's storefront that belongs to all station
+  /// genres in the Apple Music catalog.
+  ///
+  /// Example usage:
+  ///
+  ///     do {
+  ///       let countryStations = try await MCatalog.allCountryStations()
+  ///       for station in countryStations {
+  ///         print(station.name)
+  ///       }
+  ///     } catch {
+  ///       print("Error fetching stations: (error.localizedDescription)")
+  ///     }
   ///
   /// - Returns: `Stations` representing the list of stations belonging to all the station genres.
   static func allCountryStations() async throws -> Stations {
@@ -124,9 +177,20 @@ public extension MCatalog {
 
 // MARK: - PERSONAL STATION
 public extension MCatalog {
+
   /// Fetch the user's personalized Apple Music station.
   ///
-  ///  - Returns: `Station` object representing the user's personalized Apple Music station.
+  /// Example usage:
+  ///
+  ///     do {
+  ///       let personalStation = try await MCatalog.personalStation()
+  ///
+  ///       print(personalStation.name) // "Rudrank Riyam's Station"
+  ///     } catch {
+  ///       print("Error fetching personal station: \(error.localizedDescription)")
+  ///     }
+  ///
+  /// - Returns: `Station` object representing the user's personalized Apple Music station.
   static func personalStation() async throws -> Station {
     let storefront = try await MusicDataRequest.currentCountryCode
     let url = try personalStationURL(for: storefront)
@@ -158,12 +222,25 @@ public extension MCatalog {
 
 // MARK: - LIVE RADIO STATIONS
 public extension MCatalog {
+
   /// Fetch the list of featured Apple Music live radio stations.
   ///
-  /// - Returns: `Stations` representing the featured Apple Music live radio stations.
+  /// This method retrieves the current list of featured live radio stations available
+  /// on Apple Music.
   ///
-  /// - Note: As of writing this method, there are three stations: **Apple Music 1**,
+  /// As of writing this method, there are three live stations available:  **Apple Music 1**,
   /// **Apple Music Hits** and **Apple Music Country**.
+  ///
+  /// Example usage:
+  ///
+  ///     do {
+  ///       let stations = try await MCatalog.liveStations()
+  ///       print(stations) // Stations(Apple Music 1, Apple Music Hits, Apple Music Country)
+  ///     } catch {
+  ///       print("Error fetching live stations: \(error.localizedDescription)")
+  ///     }
+  ///
+  /// - Returns: `Stations` representing the featured Apple Music live radio stations.
   static func liveStations() async throws -> Stations {
     let storefront = try await MusicDataRequest.currentCountryCode
     let url = try liveStationsURL(for: storefront)
