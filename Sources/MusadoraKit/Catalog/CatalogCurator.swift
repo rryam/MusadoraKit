9b27e1bd347c5df041539987ec6ae8973298074f
Rudrank Riyam <rudrankriyam@gmail.com>
2023-07-18 01:30:37 +0800

    Update catalog curator method examples

--- Sources/MusadoraKit/Catalog/CatalogCurator.swift
+++ Sources/MusadoraKit/Catalog/CatalogCurator.swift
@@ -1,4 +1,7 @@
 //
+// "She is like writing the code that is always correct,
+//  Without her, I am just living life as a losing bet,"
+//
 //  CatalogCurator.swift
 //  MusadoraKit
 //
@@ -9,6 +12,13 @@
 public extension MCatalog {
 
   /// Fetch a curator from the Apple Music catalog by using its identifier.
+  ///
+  /// In the following example, the method fetches the details of the curator **Zane Lowe** with the ID `991490326`.
+  /// It also fetches additional relationships like `playlists` in the same request, by specifying them in the `fetch` parameter:
+  ///
+  ///     let id: MusicItemID = "991490326"
+  ///     let curator = try await MCatalog.curator(id: id, fetch: [.playlists])
+  ///
   /// - Parameters:
   ///   - id: The unique identifier for the curator.
   ///   - properties: Additional relationships to fetch with the curator.
@@ -24,10 +34,17 @@ public extension MCatalog {
     return curator
   }
 
-  /// Fetch a curator from the Apple Music catalog by using its identifier with all properties.
+  /// Fetch a curator from the Apple Music catalog by using its identifier, fetching all related properties.
+  ///
+  /// In the following example, the method fetches the details of the curator **Zane Lowe** with the ID `991490326`.
+  /// By default, it fetches all properties related to the curator in the same request:
+  ///
+  ///     let id: MusicItemID = "991490326"
+  ///     let curator = try await MCatalog.curator(id: id)
+  ///
   /// - Parameters:
   ///   - id: The unique identifier for the curator.
-  /// - Returns: `Curator` matching the given identifier.
+  /// - Returns: `Curator` matching the given identifier along with all its related properties.
   static func curator(id: MusicItemID) async throws -> Curator {
     var request = MusicCatalogResourceRequest<Curator>(matching: \.id, equalTo: id)
     request.properties = .all
@@ -40,8 +57,15 @@ public extension MCatalog {
   }
 
   /// Fetch multiple curators from the Apple Music catalog by using their identifiers.
+  ///
+  /// In the following example, the method fetches the details of curators **Zane Lowe** and **Ebro Darden** with the IDs `991490326` and `976439455` respectively.
+  /// It also fetches additional relationships like `playlists` in the same request, by specifying them in the `fetch` parameter:
+  ///
+  ///     let ids: [MusicItemID] = ["991490326", "976439455"]
+  ///     let curators = try await MCatalog.curators(ids: ids, fetch: [.playlists])
+  ///
   /// - Parameters:
-  ///   - ids: The unique identifiers for the curators.
+  ///   - ids: An array of unique identifiers for the curators.
   ///   - properties: Additional relationships to fetch with the curators.
   /// - Returns: `Curators` matching the given identifiers.
   static func curators(ids: [MusicItemID], fetch properties: CuratorProperties) async throws -> Curators {
@@ -51,10 +75,17 @@ public extension MCatalog {
     return response.items
   }
 
-  /// Fetch multiple curators from the Apple Music catalog by using their identifiers with all properties.
+  /// Fetch multiple curators from the Apple Music catalog by using their identifiers, fetching all related properties.
+  ///
+  /// In the following example, the method fetches the details of curators **Zane Lowe** and **Ebro Darden** with the IDs `991490326` and `976439455` respectively.
+  /// By default, it fetches all properties related to the curators in the same request:
+  ///
+  ///     let ids: [MusicItemID] = ["991490326", "976439455"]
+  ///     let curators = try await MCatalog.curators(ids: ids)
+  ///
   /// - Parameters:
-  ///   - ids: The unique identifiers for the curators.
-  /// - Returns: `Curators` matching the given identifiers.
+  ///   - ids: An array of unique identifiers for the curators.
+  /// - Returns: `Curators` matching the given identifiers along with all their related properties.
   static func curators(ids: [MusicItemID]) async throws -> Curators {
     var request = MusicCatalogResourceRequest<Curator>(matching: \.id, memberOf: ids)
     request.properties = .all
