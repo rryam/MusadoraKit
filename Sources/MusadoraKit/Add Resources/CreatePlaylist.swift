dee3a2d2cd52a14109e56be7aa7773de641d449e
Rudrank Riyam <rudrankriyam@gmail.com>
2023-09-14 10:22:53 +0700

    Update for macCatalyst 17.0

--- Sources/MusadoraKit/Add Resources/CreatePlaylist.swift
+++ Sources/MusadoraKit/Add Resources/CreatePlaylist.swift
@@ -12,7 +12,7 @@
 
 import Foundation
 
-@available(macOS 14.0, *)
+@available(macCatalyst 17.0, macOS 14.0, *)
 public extension MLibrary {
 
   /// Creates a new playlist in the user’s music library.
@@ -389,7 +389,7 @@ public extension MLibrary {
   ///   - playlist: A `Playlist` object representing the playlist to which the songs should be added.
   /// - Returns: An updated `Playlist` object.
   /// - Throws: An error if the item could not be added to the playlist.
-  @available(macOS 14.0, *)
+  @available(macCatalyst 17.0, macOS 14.0, *)
   @discardableResult
   static func add(item: some MusicPlaylistAddable, to playlist: Playlist) async throws -> Playlist {
     try await MusicLibrary.shared.add(item, to: playlist)
