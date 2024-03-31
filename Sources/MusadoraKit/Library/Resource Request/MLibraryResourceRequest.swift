0cd387a14ac56e58a83a82fab21b76d9779d382b
Rudrank Riyam <rudrankriyam@gmail.com>
2024-03-31 12:30:00 +0530

    Update MLibraryResourceRequest

--- Sources/MusadoraKit/Library/Resource Request/MLibraryResourceRequest.swift
+++ Sources/MusadoraKit/Library/Resource Request/MLibraryResourceRequest.swift
@@ -13,32 +13,32 @@ struct MLibraryResourceRequest<MusicItemType: MusicItem & Codable> {
   /// A limit for the number of items to return
   /// in the catalog resource response.
   var limit: Int?
-  
+
   /// Creates a request to fetch all the items in alphabetical order.
   init() {
     setType()
   }
-  
+
   /// Creates a request to fetch items using a filter that matches
   /// a specific value.
   init<Value>(matching _: KeyPath<MusicItemType.FilterLibraryType, Value>, equalTo value: Value) where MusicItemType: FilterableLibraryItem {
     setType()
-    
+
     if let id = value as? MusicItemID {
       ids = [id.rawValue]
     }
   }
-  
+
   /// Creates a request to fetch items using a filter that matches
   /// any value from an array of possible values.
   init<Value>(matching _: KeyPath<MusicItemType.FilterLibraryType, Value>, memberOf values: [Value]) where MusicItemType: FilterableLibraryItem {
     setType()
-    
+
     if let ids = values as? [MusicItemID] {
       self.ids = ids.map { $0.rawValue }
     }
   }
-  
+
   /// Fetches items from the user's library that match a specific filter.
   func response() async throws -> MLibraryResourceResponse<MusicItemType> {
     let url = try libraryEndpointURL
@@ -72,27 +72,31 @@ extension MLibraryResourceRequest {
       default: type = nil
     }
   }
-  
+
   internal var libraryEndpointURL: URL {
     get throws {
       guard let type = type else { throw URLError(.badURL) }
-      
+
       var components = AppleMusicURLComponents()
-      var queryItems: [URLQueryItem]?
+      var queryItems: [URLQueryItem] = []
       components.path = "me/library/\(type.rawValue)"
-      
+
       if let ids = ids {
-        queryItems = [URLQueryItem(name: "ids", value: ids.joined(separator: ","))]
-      } else if let limit = limit {
-        queryItems = [URLQueryItem(name: "limit", value: "\(limit)")]
+        queryItems += [URLQueryItem(name: "ids", value: ids.joined(separator: ","))]
       }
-      
+
+      if let limit = limit {
+        queryItems += [URLQueryItem(name: "limit", value: "\(limit)")]
+      }
+
+      queryItems += [URLQueryItem(name: "fields[albums]", value: "artistName,artistUrl,artwork,contentRating,editorialArtwork,name,playParams,releaseDate,url")]
+
       components.queryItems = queryItems
-      
+
       guard let url = components.url else {
         throw URLError(.badURL)
       }
-      
+
       return url
     }
   }
