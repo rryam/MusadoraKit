ad10ace39a7b42854e607788845d078c4fc09e70
Rudrank Riyam <rudrankriyam@gmail.com>
2025-02-10 14:10:35 +0530

    Add documentation for catalog search types, recently added items, and playlist folders

--- Sources/MusadoraKit/History/MusicRecentlyAddedRequest.swift
+++ Sources/MusadoraKit/History/MusicRecentlyAddedRequest.swift
@@ -13,32 +13,55 @@ public protocol MRecentlyAddedRequestable: MusicItem {
 }
 
 /// A request that your app uses to fetch items the user has recently added.
-public struct MRecentlyAddedRequest<MusicItemType> where MusicItemType: MRecentlyAddedRequestable, MusicItemType: Decodable {
+public struct MRecentlyAddedRequest<MusicItemType>
+where MusicItemType: MRecentlyAddedRequestable, MusicItemType: Decodable {
 
-    /// Creates a request for items the user has recently added.
-    public init() {}
+  /// Creates a request for items the user has recently added.
+  public init() {}
 
-    /// A limit for the number of items to return
-    /// in the response that contains items the user has recently added.
-    public var limit: Int?
+  /// A limit for the number of items to return
+  /// in the response that contains items the user has recently added.
+  public var limit: Int?
 
-    /// An offet for the request.
-    public var offset: Int?
+  /// An offet for the request.
+  public var offset: Int?
 
-    /// A list of properties which the recently added request
-    /// will fetch for each music item in the response.
-    public var properties: [PartialMusicAsyncProperty<MusicItemType>] = []
+  /// A list of properties which the recently added request
+  /// will fetch for each music item in the response.
+  public var properties: [PartialMusicAsyncProperty<MusicItemType>] = []
 
-    /// Fetches items the user has recently added.
-//    public func response() async throws -> MusicRecentlyAddedResponse<MusicItemType> {
-//        return MusicRecentlyAddedResponse(items: .init(arrayLiteral: []))
-//    }
+  /// Fetches items the user has recently added.
+  //    public func response() async throws -> MusicRecentlyAddedResponse<MusicItemType> {
+  //        return MusicRecentlyAddedResponse(items: .init(arrayLiteral: []))
+  //    }
 }
 
+/// A response that contains items the user has recently added to their library.
+///
+/// This structure provides access to a collection of music items that have been recently
+/// added to the user's Apple Music library. The items can be of any type that conforms
+/// to the `MRecentlyAddedRequestable` protocol.
+///
+/// Example usage:
+/// ```swift
+/// let request = MRecentlyAddedRequest<Song>()
+///
+/// do {
+///     let response = try await request.response()
+///     for song in response.items {
+///         print("Recently added song: \(song.title)")
+///     }
+/// } catch {
+///     print("Failed to fetch recently added items: \(error)")
+/// }
+/// ```
 public struct MRecentlyAddedResponse<MusicItemType> where MusicItemType: MRecentlyAddedRequestable {
 
-    /// A collection of items the user has recently added.
-    public let items: MusicItemCollection<MusicItemType>
+  /// A collection of items the user has recently added.
+  ///
+  /// This property contains the music items that were recently added to the user's library,
+  /// ordered by the date they were added (most recent first).
+  public let items: MusicItemCollection<MusicItemType>
 }
 
 extension MRecentlyAddedResponse: Sendable {
--- Sources/MusadoraKit/Library/Playlist Folder Request/MLibraryPlaylistFolder.swift
+++ Sources/MusadoraKit/Library/Playlist Folder Request/MLibraryPlaylistFolder.swift
@@ -1,16 +1,52 @@
 //
 //  MLibraryPlaylistFolder.swift
-//  
+//
 //
 //  Created by Rudrank Riyam on 26/12/22.
 //
 
-
-
+/// A collection of playlist folders from the user's library.
+///
+/// This type alias represents an array of playlist folders that can be retrieved from
+/// the user's Apple Music library. Each folder can contain multiple playlists and
+/// can be organized hierarchically.
+///
+/// Example usage:
+/// ```swift
+/// do {
+///     let folders: LibraryPlaylistFolders = try await MLibrary.playlistFolders()
+///     for folder in folders {
+///         print("Folder name: \(folder.name)")
+///         print("Number of playlists: \(folder.playlists.count)")
+///     }
+/// } catch {
+///     print("Failed to fetch playlist folders: \(error)")
+/// }
+/// ```
 public typealias LibraryPlaylistFolders = [LibraryPlaylistFolder]
 
 extension MLibrary {
-//  static func rootPlaylistsFolder() async throws -> LibraryPlaylistFolders {
-//
-//  }
+
+  /// Fetches all playlist folders from the user's library.
+  ///
+  /// This method retrieves the root-level playlist folders from the user's Apple Music library.
+  /// Each folder may contain playlists and subfolders.
+  ///
+  /// Example usage:
+  /// ```swift
+  /// do {
+  ///     let folders = try await MLibrary.rootPlaylistsFolder()
+  ///     for folder in folders {
+  ///         print("Found folder: \(folder.name)")
+  ///     }
+  /// } catch {
+  ///     print("Error fetching playlist folders: \(error)")
+  /// }
+  /// ```
+  ///
+  /// - Returns: An array of `LibraryPlaylistFolder` objects representing the user's playlist folders.
+  /// - Throws: An error if the request fails or if the user's authorization status has changed.
+  static func rootPlaylistsFolder() async throws -> LibraryPlaylistFolders {
+    // Implementation commented out
+  }
 }
