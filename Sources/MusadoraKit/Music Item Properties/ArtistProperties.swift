722ef53c22b1d69cd1b0fc5ea40346130b282887
Rudrank Riyam <rudrankriyam@gmail.com>
2025-10-19 01:29:14 +0530

    chore(lint): re-enable all rules; restore fatal_error custom rule scope; fix extension access modifier in ArtistProperties

--- Sources/MusadoraKit/Music Item Properties/ArtistProperties.swift
+++ Sources/MusadoraKit/Music Item Properties/ArtistProperties.swift
@@ -11,7 +11,7 @@ public typealias ArtistProperty = PartialMusicAsyncProperty<Artist>
 /// Additional properties/relationships of an artist.
 public typealias ArtistProperties = [ArtistProperty]
 
-extension ArtistProperties {
+public extension ArtistProperties {
   public static var all: Self {
     [.genres, .station, .musicVideos, .albums, .playlists, .appearsOnAlbums, .fullAlbums, .featuredAlbums, .liveAlbums, .compilationAlbums, .featuredPlaylists, .latestRelease, .topSongs, .topMusicVideos, .similarArtists, .singles]
   }
