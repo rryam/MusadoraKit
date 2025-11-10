1a7d0c32a9e3ee2a5d58d09410df763179648aee
Rudrank Riyam <rudrankriyam@gmail.com>
2025-11-10 16:47:08 +0530

    Add unit tests for InFavoritesParser

---
+++ Tests/MusadoraKitTests/Library/InFavoritesTests.swift
@@ -0,0 +1,216 @@
+@testable import MusadoraKit
+import Foundation
+import MusicKit
+import Testing
+
+@Suite
+struct InFavoritesTests {
+
+  // MARK: - Song Tests
+
+  @Test
+  func songInFavoritesParsesTrueCorrectly() throws {
+    let json = """
+    {
+      "data": [{
+        "id": "1234",
+        "type": "songs",
+        "attributes": {
+          "name": "Test Song",
+          "inFavorites": true
+        },
+        "relationships": {
+          "library": {
+            "data": [{"id": "i.abc123", "type": "library-songs"}]
+          }
+        }
+      }]
+    }
+    """
+
+    let data = json.data(using: .utf8)!
+    let inFavorites = try InFavoritesParser.parse(from: data, itemType: .song)
+
+    #expect(inFavorites == true)
+  }
+
+  @Test
+  func songInFavoritesParsesFalseCorrectly() throws {
+    let json = """
+    {
+      "data": [{
+        "id": "1234",
+        "type": "songs",
+        "attributes": {
+          "name": "Test Song",
+          "inFavorites": false
+        },
+        "relationships": {
+          "library": {
+            "data": [{"id": "i.abc123", "type": "library-songs"}]
+          }
+        }
+      }]
+    }
+    """
+
+    let data = json.data(using: .utf8)!
+    let inFavorites = try InFavoritesParser.parse(from: data, itemType: .song)
+
+    #expect(inFavorites == false)
+  }
+
+  @Test
+  func songNotInLibraryThrowsError() throws {
+    let json = """
+    {
+      "data": [{
+        "id": "1234",
+        "type": "songs",
+        "attributes": {
+          "name": "Test Song",
+          "inFavorites": false
+        },
+        "relationships": {
+          "library": {
+            "data": []
+          }
+        }
+      }]
+    }
+    """
+
+    let data = json.data(using: .utf8)!
+
+    #expect(throws: MusadoraKitError.self) {
+      try InFavoritesParser.parse(from: data, itemType: .song)
+    }
+  }
+
+  // MARK: - Album Tests
+
+  @Test
+  func albumInFavoritesParsesTrueCorrectly() throws {
+    let json = """
+    {
+      "data": [{
+        "id": "1234",
+        "type": "albums",
+        "attributes": {
+          "name": "Test Album",
+          "inFavorites": true
+        },
+        "relationships": {
+          "library": {
+            "data": [{"id": "i.xyz789", "type": "library-albums"}]
+          }
+        }
+      }]
+    }
+    """
+
+    let data = json.data(using: .utf8)!
+    let inFavorites = try InFavoritesParser.parse(from: data, itemType: .album)
+
+    #expect(inFavorites == true)
+  }
+
+  // MARK: - Artist Tests
+
+  @Test
+  func artistInFavoritesParsesTrueCorrectly() throws {
+    let json = """
+    {
+      "data": [{
+        "id": "1234",
+        "type": "artists",
+        "attributes": {
+          "name": "Test Artist",
+          "inFavorites": true
+        },
+        "relationships": {
+          "library": {
+            "data": [{"id": "i.def456", "type": "library-artists"}]
+          }
+        }
+      }]
+    }
+    """
+
+    let data = json.data(using: .utf8)!
+    let inFavorites = try InFavoritesParser.parse(from: data, itemType: .artist)
+
+    #expect(inFavorites == true)
+  }
+
+  // MARK: - Playlist Tests
+
+  @Test
+  func playlistInFavoritesParsesTrueCorrectly() throws {
+    let json = """
+    {
+      "data": [{
+        "id": "pl.123",
+        "type": "playlists",
+        "attributes": {
+          "name": "Test Playlist",
+          "inFavorites": true
+        },
+        "relationships": {
+          "library": {
+            "data": [{"id": "pl.u-abc", "type": "library-playlists"}]
+          }
+        }
+      }]
+    }
+    """
+
+    let data = json.data(using: .utf8)!
+    let inFavorites = try InFavoritesParser.parse(from: data, itemType: .playlist)
+
+    #expect(inFavorites == true)
+  }
+
+  // MARK: - Error Case Tests
+
+  @Test
+  func missingInFavoritesAttributeThrowsError() throws {
+    let json = """
+    {
+      "data": [{
+        "id": "1234",
+        "type": "songs",
+        "attributes": {
+          "name": "Test Song"
+        },
+        "relationships": {
+          "library": {
+            "data": [{"id": "i.abc123", "type": "library-songs"}]
+          }
+        }
+      }]
+    }
+    """
+
+    let data = json.data(using: .utf8)!
+
+    #expect(throws: MusadoraKitError.self) {
+      try InFavoritesParser.parse(from: data, itemType: .song)
+    }
+  }
+
+  @Test
+  func emptyDataArrayThrowsError() throws {
+    let json = """
+    {
+      "data": []
+    }
+    """
+
+    let data = json.data(using: .utf8)!
+
+    #expect(throws: MusadoraKitError.self) {
+      try InFavoritesParser.parse(from: data, itemType: .song)
+    }
+  }
+}
